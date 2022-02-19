21d27e1550abf1199fb9ee05c5d11c1391d5280c
Rajib Mondal <mondal021002rajib@gmail.com>
2022-02-19 11:40:20 +0530

    mobile navbar completed

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
